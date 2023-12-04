363fe4c70709780618fede76512b2a4d3a8c086b
fzdwx <likelovec@gmail.com>
2023-12-04 23:19:20 +0800

    chore: use electron

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import '@fzdwx/launcher-api/dist/style.css'
-import './assets/main.scss'
-
-import {createApp} from 'vue'
-// @ts-ignore
-import App from './App.vue'
-
-let app = createApp(App);
-app.mount('#app')
